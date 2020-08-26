4dbb06ad0242799606d36520ba246851b45c8f19
joserosalesdh <jm.rosales92@gmail.com>
2020-08-26 12:59:30 -0300

    comir

---
+++ docs/precache-manifest.aecd800cde66489db6597de58b81b21a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "24838d9a9cbcfcb2f89c218152272c78",
+    "url": "/index.html"
+  },
+  {
+    "revision": "64cc2b319dd1f8a7af97",
+    "url": "/static/css/main.67e58c6c.chunk.css"
+  },
+  {
+    "revision": "7ded67fd27fde8d3a393",
+    "url": "/static/js/2.281275a3.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.281275a3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "64cc2b319dd1f8a7af97",
+    "url": "/static/js/main.ddf6a634.chunk.js"
+  },
+  {
+    "revision": "73635f645c271e35ff15",
+    "url": "/static/js/runtime-main.83c3e0c4.js"
+  }
+]);
